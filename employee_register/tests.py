29deaf04591cd972b8a258b8e08796eeb9c09fdd
Ashek-Al- Mahady <50077@palonet.org>
2026-01-01 15:02:15 +0600

    Fix emp_code length in tests

--- employee_register/tests.py
+++ employee_register/tests.py
@@ -1,21 +1,64 @@
 from django.test import TestCase
+from django.urls import reverse
 from employee_register.models import Employee, Position
 
 #Unit Test- Code behaviour test
 
 class EmployeeTest(TestCase):
 
-    def test_employee_creation(self): #Checking data in creating thourgh this code or not
-        position = Position.objects.create(title="Manager") #Checing ORM mapping
 
+    # Create
+    def setUp(self):
+        self.position = Position.objects.create(title="Manager") #Checing ORM mapping
+
+    def test_employee_creation(self):
         emp = Employee.objects.create(
             fullname="Ashek",
             emp_code="707",
             mobile="01900000000",
-            position=position #Checking ForeignKey relation
+            position=self.position
         )
 
-        self.assertEqual(emp.position.id, position.id)
+        self.assertEqual(emp.position.id, self.position.id)
         self.assertEqual(emp.position.title, "Manager")
-        #self.assertEqual(1, 2)
+    
+    #Update
+    def test_employee_update(self):
+        emp = Employee.objects.create(
+            fullname="Ashek",
+            emp_code="707",
+            mobile="01900000000",
+            position=self.position
+        )
+
+        response = self.client.post(
+            reverse("employee_update", args=[emp.id]),
+            {
+                "fullname": "Mahady",
+                "emp_code": "002",
+                "mobile": "01800000000",
+                "position": self.position.id,
+            }
+        )
+
+        emp.refresh_from_db()
+        self.assertEqual(emp.fullname, "Mahady")
+
+    # Delete
+    def test_employee_delete(self):
+        emp = Employee.objects.create(
+            fullname="Mahady",
+            emp_code="202",
+            mobile="01800000000",
+            position=self.position
+        )
+
+        response = self.client.post(
+            reverse("employee_delete", args=[emp.id])
+        )
+
+        self.assertEqual(Employee.objects.count(), 0)
+
+
+
 
