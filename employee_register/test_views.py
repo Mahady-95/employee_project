804aea70edb4422c7318bf08f63ca532ba790d96
Ashek-Al- Mahady <50077@palonet.org>
2026-01-01 12:45:08 +0600

    optimized test code

--- employee_register/test_views.py
+++ employee_register/test_views.py
@@ -1,8 +1,22 @@
-from django.test import TestCase
+# from django.test import TestCase
+# from django.urls import reverse
+
+# class EmployeeViewTest(TestCase):
+
+#     def test_employee_list_page_status(self):
+#         response = self.client.get(reverse("employee_list"))
+#         self.assertEqual(response.status_code, 200)
+
 from django.urls import reverse
+from django.test import TestCase
+from employee_register.models import Employee
 
-class EmployeeViewTest(TestCase):
-
-    def test_employee_list_page_status(self):
-        response = self.client.get(reverse("employee_list"))
-        self.assertEqual(response.status_code, 200)
+class EmployeeCreateTest(TestCase):
+    def test_create_employee(self):
+        response = self.client.post(reverse('employee_create'), {
+            'fullname': 'Test User',
+            'emp_code': 'EMP01',
+            'mobile': '01700000000'
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Employee.objects.count(), 1)
