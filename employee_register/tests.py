7f204ac0f46a6fe9c676ae656335ba0c747e5732
Ashek-Al- Mahady <50077@palonet.org>
2026-01-08 17:37:05 +0600

    test login case added

--- employee_register/tests.py
+++ employee_register/tests.py
@@ -1,16 +1,86 @@
+# from django.test import TestCase
+# from django.urls import reverse
+# from employee_register.models import Employee, Position
+
+# #Unit Test- Code behaviour test
+
+# class EmployeeTest(TestCase):
+
+
+#     # Create
+#     def setUp(self):
+#         self.position = Position.objects.create(title="HR") #Checking ORM mapping
+
+#     def test_employee_creation(self):
+#         emp = Employee.objects.create(
+#             fullname="Chorki Apple TV",
+#             emp_code="707",
+#             mobile="01900000000",
+#             position=self.position
+#         )
+
+#         self.assertEqual(emp.position.id, self.position.id)
+#         self.assertEqual(emp.position.title, "HR")
+    
+#     #Update
+#     def test_employee_update(self):
+#         emp = Employee.objects.create(
+#             fullname="Ashek",
+#             emp_code="707",
+#             mobile="01900000000",
+#             position=self.position
+#         )
+
+#         response = self.client.post(
+#             reverse("employee_update", args=[emp.id]),
+#             {
+#                 "fullname": "Mahady",
+#                 "emp_code": "002",
+#                 "mobile": "01800000000",
+#                 "position": self.position.id,
+#             }
+#         )
+
+#         emp.refresh_from_db()
+#         self.assertEqual(emp.fullname, "Mahady")
+
+#     # Delete
+#     def test_employee_delete(self):
+#         emp = Employee.objects.create(
+#             fullname="Mahady",
+#             emp_code="202",
+#             mobile="01800000000",
+#             position=self.position
+#         )
+
+#         response = self.client.post(
+#             reverse("employee_delete", args=[emp.id])
+#         )
+
+#         self.assertEqual(Employee.objects.count(), 0)
+
+
+
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth.models import User
 from employee_register.models import Employee, Position
 
-#Unit Test- Code behaviour test
 
 class EmployeeTest(TestCase):
 
-
-    # Create
     def setUp(self):
-        self.position = Position.objects.create(title="HR") #Checking ORM mapping
+        # 🔐 Create & login user (REQUIRED for login_required views)
+        self.user = User.objects.create_user(
+            username="testuser",
+            password="testpass123"
+        )
+        self.client.login(username="testuser", password="testpass123")
 
+        # 🔹 Create position
+        self.position = Position.objects.create(title="HR")
+
+    # ✅ CREATE
     def test_employee_creation(self):
         emp = Employee.objects.create(
             fullname="Chorki Apple TV",
@@ -21,8 +91,9 @@ class EmployeeTest(TestCase):
 
         self.assertEqual(emp.position.id, self.position.id)
         self.assertEqual(emp.position.title, "HR")
-    
-    #Update
+        self.assertEqual(Employee.objects.count(), 1)
+
+    # ✅ UPDATE
     def test_employee_update(self):
         emp = Employee.objects.create(
             fullname="Ashek",
@@ -41,10 +112,15 @@ class EmployeeTest(TestCase):
             }
         )
 
+        # 🔹 Ensure redirect after successful update
+        self.assertEqual(response.status_code, 302)
+
         emp.refresh_from_db()
         self.assertEqual(emp.fullname, "Mahady")
+        self.assertEqual(emp.emp_code, "002")
+        self.assertEqual(emp.mobile, "01800000000")
 
-    # Delete
+    # ✅ DELETE
     def test_employee_delete(self):
         emp = Employee.objects.create(
             fullname="Mahady",
@@ -57,8 +133,6 @@ class EmployeeTest(TestCase):
             reverse("employee_delete", args=[emp.id])
         )
 
+        self.assertEqual(response.status_code, 302)
         self.assertEqual(Employee.objects.count(), 0)
-
-
-
 
