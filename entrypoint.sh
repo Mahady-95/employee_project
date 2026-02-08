07475418df67f7a303a982044c7c29f99190883e
Ashek-Al- Mahady <50077@palonet.org>
2026-02-08 11:59:37 +0600

    fix: start gunicorn without blocking for Render

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,24 @@
+# #!/bin/sh
+# set -e
+
+# echo "Waiting for database..."
+# until nc -z "$DB_HOST" "$DB_PORT"; do
+#   sleep 1
+# done
+# echo "Database is ready"
+
+# python manage.py migrate --noinput
+# python manage.py collectstatic --noinput
+
+# exec "$@"
 #!/bin/sh
 set -e
 
-echo "Waiting for database..."
-until nc -z "$DB_HOST" "$DB_PORT"; do
-  sleep 1
-done
-echo "Database is ready"
-
+echo "Running migrations..."
 python manage.py migrate --noinput
+
+echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
+echo "Starting server..."
 exec "$@"
