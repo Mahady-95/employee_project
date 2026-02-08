9026465fe8a12166ee2ade272626b0c38087462c
Ashek-Al- Mahady <50077@palonet.org>
2026-02-08 16:56:22 +0600

    fix: dockerfile & entrysh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+# entrypoint.sh
 
-# wait for db to be ready
-echo "Waiting for MySQL..."
-while ! mysqladmin ping -h"$DB_HOST" --silent; do
-    sleep 1
-done
-
-# run migrations
-echo "Running migrations..."
+# Apply migrations
 python manage.py migrate
 
-# collect static files
-echo "Collecting static files..."
+# Collect static files
 python manage.py collectstatic --noinput
 
-# start server
-echo "Starting server..."
-python manage.py runserver 0.0.0.0:8000
-
+# Start server on all interfaces, port from $PORT
+exec gunicorn employee_project.wsgi:application \
+    --bind 0.0.0.0:${PORT:-8000} \
+    --workers 2
