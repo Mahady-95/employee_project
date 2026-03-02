aef91523e4aea20a963e05611c2af23663d0aedb
Ashek-Al- Mahady <50077@palonet.org>
2026-03-02 15:20:43 +0600

    Stage Deploy

--- employee_project/settings.py
+++ employee_project/settings.py
@@ -106,6 +106,7 @@ else:
                 "charset": "utf8mb4",
                 "use_unicode": True,
                 "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+                "ssl": {"check_hostname": False, "verify_cert": False},  # ✅ skip SSL
             },
         }
     }
